cf4ea06172026993485f098b18d66f4c46d9f869
GabrielGardev <gabriel.gurdev@gmail.com>
2020-01-28 22:56:48 +0200

    added solutions

--- JS Advanced/JS Advanced/5. Objects and DOM/2. Exercise/04. Numpad-Calculator/solution.js
+++ JS Advanced/JS Advanced/5. Objects and DOM/2. Exercise/04. Numpad-Calculator/solution.js
@@ -1,4 +1,39 @@
 function solve() {
+    let expresion = document.getElementById('expressionOutput')
+    let result = document.getElementById('resultOutput')
+    let pad = document.querySelector('.keys')
+    let clearButton = document.querySelector('.clear')
 
-    //TODO...
+    let operators = {
+        '/': (left, right) => left / right,
+        'x': (left, right) => left * right,
+        '-': (left, right) => left - right,
+        '+': (left, right) => left + right
+    }
+
+    clearButton.addEventListener('click', () =>{
+        expresion.innerHTML = '';
+        result.innerHTML = '';
+        return
+    })
+
+    pad.addEventListener('click', ({ target: {innerHTML}}) => {
+        let currentSymbol = innerHTML;
+
+        if (currentSymbol === '=') {
+            let arr = expresion.innerHTML.split(' ')
+            if(arr[2] === ''){
+                result.innerHTML = NaN
+                return
+            }
+            result.innerHTML = operators[arr[1]](+arr[0], +arr[2])
+            return
+        }
+        
+        if(currentSymbol >= 0 && currentSymbol <= 9 || currentSymbol === '.'){
+            expresion.innerHTML += currentSymbol
+            return
+        }
+        expresion.innerHTML += " " + currentSymbol + " ";
+    })
 }
--- JS Advanced/JS Advanced/5. Objects and DOM/2. Exercise/05. Order-the-Names/solution.js
+++ JS Advanced/JS Advanced/5. Objects and DOM/2. Exercise/05. Order-the-Names/solution.js
@@ -1,4 +1,25 @@
 function solve() {
+    let input = document.getElementsByTagName("input")[0];
+    let button = document.querySelector("button");
+    let ol = document.querySelector("ol");
 
-    //TODO...
+    const capitalize = (s) => {
+        if (typeof s !== 'string') return ''
+        return s.charAt(0).toUpperCase() + s.slice(1)
+      }
+
+    button.addEventListener('click', () => {
+        let word = input.value.toLowerCase();
+        let position = word.charCodeAt(0) - 97;
+        let currentNames = ol.children[position].innerHTML;
+
+        let currentValues = []
+        if(currentNames !== ""){
+            currentValues = currentNames.split(', ')
+        }
+        currentValues.push(capitalize(word))
+        ol.children[position].innerHTML = currentValues.join(', ')
+
+        input.value = ''
+    })
 }
