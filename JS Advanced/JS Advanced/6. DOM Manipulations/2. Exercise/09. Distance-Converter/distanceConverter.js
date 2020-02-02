912f3e0adb0c4aa83b823982e6f3c190b3b80023
GabrielGardev <gabriel.gurdev@gmail.com>
2020-02-02 15:56:07 +0200

    task added

--- JS Advanced/JS Advanced/6. DOM Manipulations/2. Exercise/09. Distance-Converter/distanceConverter.js
+++ JS Advanced/JS Advanced/6. DOM Manipulations/2. Exercise/09. Distance-Converter/distanceConverter.js
@@ -1,3 +1,19 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    let button = document.getElementById('convert');
+    button.addEventListener('click', convert);
+
+    let units = [1000, 1, 0.01, 0.001, 1609.34, 0.9144, 0.3048, 0.0254];
+
+    function convert() {
+        let input = document.getElementById('inputDistance').value;
+
+        if (/[+-]*?[0-9]+(?:\.[0-9]+)?/.test(input)) {
+            let number = Number(input);
+            let inputUnit = document.getElementById('inputUnits').selectedIndex;
+            let outputUnit = document.getElementById('outputUnits').selectedIndex;
+
+            let inValue = number * units[inputUnit];
+            document.getElementById('outputDistance').value = inValue / units[outputUnit];
+        }
+    }
 }
