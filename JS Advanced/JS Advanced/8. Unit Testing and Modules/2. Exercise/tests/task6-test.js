17bcd3c8ea7f8d920b4b82a9177be59dfe9c1176
GabrielGardev <gabriel.gurdev@gmail.com>
2020-02-11 11:16:12 +0200

    refactor task6

--- JS Advanced/JS Advanced/8. Unit Testing and Modules/2. Exercise/tests/task6-test.js
+++ JS Advanced/JS Advanced/8. Unit Testing and Modules/2. Exercise/tests/task6-test.js
@@ -12,21 +12,8 @@ describe("PaymentPackage behavior", function () {
                 _active: true
             });
         });
-        it("one param should throw error", function () {
-            let pp = () => new PaymentPackage('HR Services');
-            assert.throws(pp, 'Value must be a non-negative number');
-        });
-        it("0 params should throw error", function () {
-            let pp = () => new PaymentPackage();
-            assert.throws(pp, 'Name must be a non-empty string');
-        });
     });
     describe("toString check", () => {
-        it("toString should return correct", () => {
-            let pp = new PaymentPackage('HR Services', 1500);
-            assert.equal(pp.toString(), "Package: HR Services\n- Value (excl. VAT): 1500\n- Value (VAT 20%): 1800")
-        });
-
         it("toString should return correct with 0", () => {
             let pp = new PaymentPackage('HR Services', 0);
             assert.equal(pp.toString(), "Package: HR Services\n- Value (excl. VAT): 0\n- Value (VAT 20%): 0")
