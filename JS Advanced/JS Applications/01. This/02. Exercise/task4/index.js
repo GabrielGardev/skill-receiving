0f6e2aa5f3d92ac99a7b6a75dc58862f6d83db9f
GabrielGardev <gabriel.gurdev@gmail.com>
2020-02-25 16:04:32 +0200

    exercise added

--- JS Advanced/JS Applications/01. This/02. Exercise/task4/index.js
+++ JS Advanced/JS Applications/01. This/02. Exercise/task4/index.js
@@ -1,3 +1,19 @@
 function solve(){
-   // TODO
+   Array.from(
+      document.getElementsByTagName('tr')
+   ).slice(1)
+      .forEach(tr => {
+         tr.addEventListener('click', function() {
+            if(this.hasAttribute('style')){
+               this.removeAttribute('style')
+            } else {
+               Array.from(this.parentElement.children)
+                  .forEach(row => {
+                     row.removeAttribute('style');
+                  });
+
+               this.style.backgroundColor = "#413f5e";
+            }
+         });
+      });
 }
