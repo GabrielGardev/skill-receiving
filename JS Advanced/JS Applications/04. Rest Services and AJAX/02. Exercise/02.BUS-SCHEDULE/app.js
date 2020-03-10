6283d70157c7a55951c0f7c67345c0e8d360eee5
GabrielGardev <gabriel.gurdev@gmail.com>
2020-03-10 21:12:30 +0200

    exercise added

--- JS Advanced/JS Applications/04. Rest Services and AJAX/02. Exercise/02.BUS-SCHEDULE/app.js
+++ JS Advanced/JS Applications/04. Rest Services and AJAX/02. Exercise/02.BUS-SCHEDULE/app.js
@@ -1,11 +1,30 @@
 function solve() {
+    let info = document.querySelector('.info');
+    let btnDepart = document.querySelector('#depart');
+    let btnArrive = document.querySelector('#arrive');
+
+    let [currentId, currentName] = ['depot', '']
 
     function depart() {
-        console.log('Depart TODO...');
+        fetch(`https://rest-service-and-ajax.firebaseio.com/schedule/${currentId}.json`)
+            .then(x => x.json())
+            .then(res => {
+                btnDepart.disabled = true;
+                btnArrive.disabled = false;
+                [currentId, currentName] = [res.next, res.name];
+                info.textContent = `Next stop ${currentName}`;
+            })
+            .catch(() => {
+                btnDepart.disabled = true;
+                btnArrive.disabled = true;
+                info.textContent = `Error`
+            })
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        btnDepart.disabled = false;
+        btnArrive.disabled = true;
+        info.textContent = `Arriving at ${currentName}`
     }
 
     return {
--- JS Advanced/JS Applications/04. Rest Services and AJAX/02. Exercise/03.PHONEBOOK/app.js
+++ JS Advanced/JS Applications/04. Rest Services and AJAX/02. Exercise/03.PHONEBOOK/app.js
@@ -1,5 +1,65 @@
 function attachEvents() {
-    console.log('TODO...');
+    let ul = document.querySelector('#phonebook');
+    let personInput = document.querySelector('#person');
+    let phoneInput = document.querySelector('#phone');
+
+    function load() {
+        ul.innerHTML = ''
+        fetch('https://rest-service-and-ajax.firebaseio.com/phonebook.json')
+            .then(x => x.json())
+            .then(res => {
+                Object.entries(res).forEach(([elId, data]) => {
+                    if (data) {
+                        let li = document.createElement('li');
+                        li.textContent = `${data.person}: ${data.phone}`
+
+                        let btn = document.createElement('button');
+                        btn.textContent = 'Delete';
+                        btn.setAttribute('data-target', elId);
+                        btn.addEventListener('click', deleteEvent);
+                        li.appendChild(btn);
+
+                        ul.appendChild(li);
+                    }
+                });
+            })
+            .catch(handelError)
+    }
+
+    function create() {
+        let [person, phone] = [personInput.value, phoneInput.value]
+
+        fetch('https://rest-service-and-ajax.firebaseio.com/phonebook.json', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ person, phone })
+        })
+            .then(() => {
+                ul.innerHTML = ''
+                personInput.value = ''
+                phoneInput.value = ''
+
+                load()
+            })
+            .catch(handelError)
+    }
+
+    function handelError(err) {
+        console.log(err)
+    }
+
+    function deleteEvent() {
+        fetch(`https://rest-service-and-ajax.firebaseio.com/phonebook/${this.getAttribute("data-target")}.json`, {
+            method: 'DELETE'
+        })
+        .then(() => {
+            ul.innerHTML = ''
+            load();
+        })
+        .catch(handelError)
+    }
+
+    return { load, create }
 }
 
-attachEvents();
+let result = attachEvents();
