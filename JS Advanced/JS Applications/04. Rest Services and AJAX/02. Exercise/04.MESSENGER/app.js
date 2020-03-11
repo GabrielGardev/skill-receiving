e44f7e1d004fd9a592e21e249599f7123514b09f
GabrielGardev <gabriel.gurdev@gmail.com>
2020-03-11 11:50:04 +0200

    exercise added

--- JS Advanced/JS Applications/04. Rest Services and AJAX/02. Exercise/04.MESSENGER/app.js
+++ JS Advanced/JS Applications/04. Rest Services and AJAX/02. Exercise/04.MESSENGER/app.js
@@ -1,5 +1,46 @@
 function attachEvents() {
-    console.log('TODO...');
+    let messages = document.getElementById('messages')
+    let inputA = document.getElementById('author')
+    let inputC = document.getElementById('content')
+    let btnSent = document.getElementById('submit')
+    let btnRefresh = document.getElementById('refresh')
+
+    btnSent.addEventListener('click', postReq)
+    btnRefresh.addEventListener('click', getReq)
+
+    function getReq() {
+        fetch('https://bomb-cd5af.firebaseio.com/messenger.json')
+            .then(x => x.json())
+            .then(res => {
+                messages.textContent = "";
+
+                const result = [];
+                Object.entries(res).forEach(([_, data]) => {
+                    result.push(`${data.author}: ${data.content}`)                   
+                })
+                messages.textContent = result.join("\n");
+            })
+            .catch(handelError)
+    }
+
+    function postReq() {
+        [author, content] = [inputA.value, inputC.value]
+        const obj = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ author, content })
+        }
+        fetch('https://bomb-cd5af.firebaseio.com/messenger.json', obj)
+            .then(() => {
+                inputA.value = ''
+                inputC.value = ''
+            })
+            .catch(handelError)
+    }
+
+    function handelError(err) {
+        console.log(err)
+    }
 }
 
 attachEvents();
