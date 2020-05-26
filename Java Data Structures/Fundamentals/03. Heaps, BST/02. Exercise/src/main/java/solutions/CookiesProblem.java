2120c3c1b0c3c90c7e3bf38ea4ced9659f3b924d
GabrielGardev <gabriel.gurdev@gmail.com>
2020-05-26 13:28:34 +0300

    added exercise

--- Java Data Structures/Fundamentals/03. Heaps, BST/02. Exercise/src/main/java/solutions/CookiesProblem.java
+++ Java Data Structures/Fundamentals/03. Heaps, BST/02. Exercise/src/main/java/solutions/CookiesProblem.java
@@ -1,7 +1,30 @@
 package solutions;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class CookiesProblem {
-    public Integer solve(int k, int[] cookies) {
-        return null;
+    public Integer solve(int requiredSweetness, int[] cookiesSweetness) {
+        Queue<Integer> cookies = new PriorityQueue<>();
+
+        for (int sweetness : cookiesSweetness) {
+            cookies.add(sweetness);
+        }
+
+        int currentMinSweetness = cookies.peek();
+        int counter = 0;
+
+        while (currentMinSweetness < requiredSweetness && cookies.size() > 1){
+            int leastSweetCookie = cookies.poll();
+            int secondLeastSweetCookie = cookies.poll();
+
+            int combined = leastSweetCookie + (2 * secondLeastSweetCookie);
+
+            cookies.add(combined);
+            counter++;
+
+            currentMinSweetness = cookies.peek();
+        }
+        return currentMinSweetness > requiredSweetness ? counter : -1;
     }
 }
