717b8b112580c3ae7043d5f85655ed09eb0e5070
GabrielGardev <gabriel.gurdev@gmail.com>
2020-06-02 18:43:40 +0300

    added exercise

--- Algorithums/01. Fundamentals/03. Recursion and Combinatorial Algorithms/src/P4_Tower_of_Hanoi.java
+++ Algorithums/01. Fundamentals/03. Recursion and Combinatorial Algorithms/src/P4_Tower_of_Hanoi.java
@@ -1,2 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.stream.Collectors;
+
 public class P4_Tower_of_Hanoi {
+    public static StringBuilder sb = new StringBuilder();
+
+    public static Deque<Integer> source = new ArrayDeque<>();
+    public static Deque<Integer> destination = new ArrayDeque<>();
+    public static Deque<Integer> spare = new ArrayDeque<>();
+    public static int steps = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int disk = Integer.parseInt(reader.readLine());
+
+        for (int i = disk; i >= 1; i--) {
+            source.push(i);
+        }
+
+        printRods();
+        solve(disk, source, destination, spare);
+        System.out.println(sb);
+    }
+
+    private static void solve(int disk, Deque<Integer> source, Deque<Integer> destination, Deque<Integer> spare) {
+        if (disk == 1){
+            destination.push(source.pop());
+            sb.append("Step #").append(++steps).append(": Moved disk").append(System.lineSeparator());
+            printRods();
+        }else {
+            solve(disk - 1, source, spare, destination);
+            solve(1, source, destination, spare);
+            solve(disk - 1, spare, destination, source);
+        }
+    }
+
+    public static void printRods(){
+        sb.append("Source: ").append(formatRod(source)).append(System.lineSeparator())
+                .append("Destination: ").append(formatRod(destination)).append(System.lineSeparator())
+                .append("Spare: ").append(formatRod(spare)).append(System.lineSeparator())
+                .append(System.lineSeparator());
+    }
+
+    public static String formatRod(Deque<Integer> stack){
+        return stack.stream()
+                .sorted(Comparator.reverseOrder())
+                .map(String::valueOf)
+                .collect(Collectors.joining(", "));
+    }
 }
---
+++ Algorithums/01. Fundamentals/03. Recursion and Combinatorial Algorithms/src/P5_Combinations_without_Repetition.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P5_Combinations_without_Repetition {
+    public static int[] numbs;
+    public static int n;
+    public static int k;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(reader.readLine());
+        k = Integer.parseInt(reader.readLine());
+
+        numbs = new int[k];
+
+        comb(0, 1);
+    }
+
+    public static void comb(int index, int start) {
+        if (index >= k) {
+            print();
+        } else {
+            for (int i = start; i <= n; i++) {
+                numbs[index] = i;
+                comb(index + 1, i + 1);
+            }
+        }
+    }
+
+    private static void print() {
+        StringBuilder sb = new StringBuilder();
+        for (int numb : numbs) {
+            sb.append(numb).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
