d34af16b41b232e8cbab93ad78b292c1cfa19206
GabrielGardev <gabriel.gurdev@gmail.com>
2020-06-08 11:15:22 +0300

    name change

--- Algorithms/01. Fundamentals/04. Searching, Sorting and Greedy Algorithms/src/P01_Binary_Search.java
+++ Algorithms/01. Fundamentals/04. Searching, Sorting and Greedy Algorithms/src/P01_Binary_Search.java
@@ -1,5 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class P01_Binary_Search {
     public static void main(String[] args) {
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     }
 }
