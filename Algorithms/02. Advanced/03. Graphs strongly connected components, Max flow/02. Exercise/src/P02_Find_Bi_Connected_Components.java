7cf9c256b0f8060bdb1885fb6d4c98e005e0d4c1
GabrielGardev <gabriel.gurdev@gmail.com>
2020-07-17 17:45:10 +0300

    Find Bi-CC added

--- Algorithms/02. Advanced/03. Graphs strongly connected components, Max flow/02. Exercise/src/P02_Find_Bi_Connected_Components.java
+++ Algorithms/02. Advanced/03. Graphs strongly connected components, Max flow/02. Exercise/src/P02_Find_Bi_Connected_Components.java
@@ -6,81 +6,61 @@ import java.util.Arrays;
 import java.util.List;
 
 public class P02_Find_Bi_Connected_Components {
-    public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     public static int[][] graph;
-    public static int nodes;
-    public static int edges;
-    public static List<List<Integer>> subGraphs = new ArrayList<>();
-    public static boolean[] visited;
     public static int[] parents;
     public static int[] depths;
     public static int[] lowPoints;
+    public static int[] reachableCount;
+    public static List<List<Integer>> supGraphs = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
-        nodes = Integer.parseInt(reader.readLine().split("\\s+")[1]);
-        edges = Integer.parseInt(reader.readLine().split("\\s+")[1]);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int nodes = Integer.parseInt(reader.readLine().split(": ")[1]);
+        int edges = Integer.parseInt(reader.readLine().split(": ")[1]);
+
         graph = new int[nodes][nodes];
-
-        fillGraph();
-        List<Integer> articulationPoints = findArticulationPoints();
-        System.out.println();
-    }
-
-    private static List<Integer> findArticulationPoints() {
-        visited = new boolean[graph.length];
-        parents = new int[graph.length];
-        depths = new int[graph.length];
-        lowPoints = new int[graph.length];
-
+        depths = new int[nodes];
+        lowPoints = new int[nodes];
+        parents = new int[nodes];
+        reachableCount = new int[nodes];
         Arrays.fill(parents, -1);
 
-        discoverArticulationPoints(0, 1);
+        for (int i = 0; i < edges; i++) {
+            int[] edgeInfo = Arrays.stream(reader.readLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            int src = edgeInfo[0];
+            int dst = edgeInfo[1];
 
-        return points;
+            graph[src][reachableCount[src]++] = dst;
+            graph[dst][reachableCount[dst]++] = src;
+        }
+
+        findArticulationPoints(0, 0, new ArrayList<>());
+        System.out.println("Number of bi-connected components: " + supGraphs.size());
     }
 
-    private static void discoverArticulationPoints(int node, int depth) {
-        visited[node] = true;
+    private static void findArticulationPoints(int node, int depth, List<Integer> subSet) {
         depths[node] = depth;
         lowPoints[node] = depth;
 
-        int children = 0;
-
-        boolean isArticulationPoint = false;
-
-        for (int child : graph[node]) {
-
-            if (child != 0 && !visited[child]) {
+        for (int i = 0; i < reachableCount[node]; i++) {
+            int child = graph[node][i];
+            if (depths[child] == 0) {
                 parents[child] = node;
-                children++;
-                discoverArticulationPoints(child, depth + 1);
-
-                if (lowPoints[child] >= depth) {
-                    isArticulationPoint = true;
+                List<Integer> components = new ArrayList<>();
+                findArticulationPoints(child, depth + 1, components);
+                if (lowPoints[child] >= depths[node] || parents[child] == -1) {
+                    components.add(node);
+                    supGraphs.add(components);
+                } else {
+                    subSet.addAll(components);
                 }
                 lowPoints[node] = Math.min(lowPoints[node], lowPoints[child]);
-            } else if (parents[node] != child) {
+            } else if (child != parents[node]) {
                 lowPoints[node] = Math.min(lowPoints[node], depths[child]);
             }
         }
-
-        if (parents[node] == -1 && children > 1 || (parents[node] != -1 && isArticulationPoint)) {
-            points.add(node);
-        }else {
-            num++;
-        }
-    }
-
-    private static void fillGraph() throws IOException {
-        for (int i = 0; i < nodes; i++) {
-            int[] tokens = Arrays.stream(reader.readLine().split("\\s+"))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-            int source = tokens[0];
-            int destination = tokens[1];
-
-            graph[source][destination] = 1;
-            graph[destination][source] = 1;
-        }
+        subSet.add(node);
     }
 }
